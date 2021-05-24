cfef10158e44643374073bc07444f888a88b5afd
Damian Grüner <grunerdamian@gmail.com>
2021-05-24 20:17:42 +0200

    columns

--- 2021-05-24--web-l4-js-angular/controller.js
+++ 2021-05-24--web-l4-js-angular/controller.js
@@ -1,1 +1,55 @@
-// angular.module
+function log(x) {
+  console.log(x);
+}
+
+angular.module("App", []).controller("Controller", function ($scope) {
+  $scope.calculation = "";
+  $scope.input = "0";
+
+  $scope.HandleInput = function (action) {
+    log("________ action: " + action);
+
+    // 0-9
+    if (!isNaN(+action)) {
+      log("is a number");
+      if ($scope.input === "0") $scope.input = action;
+      else $scope.input += action;
+    }
+
+    // .
+    if (action === ".") {
+    }
+
+    // C
+    if (action === "C") {
+      $scope.input = "0";
+      $scope.calculation = "";
+    }
+
+    // CE
+    if (action === "CE") {
+      $scope.input = "0";
+    }
+
+    // + - * /
+    ["+", "-", "*", "/"].forEach((op) => {
+      if (action === op) {
+        $scope.calculation += $scope.input;
+        $scope.calculation += op;
+        $scope.input = "0";
+      }
+    });
+
+    // .
+    if (action === "." && !$scope.input.includes(".")) {
+      $scope.input += ".";
+    }
+
+    // =
+    if (action === "=") {
+      log("evaluating...");
+      $scope.input = math.evaluate($scope.calculation + $scope.input);
+      $scope.calculation = "";
+    }
+  };
+});
